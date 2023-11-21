3a8b8bcb5013e3e839d8d2248b07356828e05e1b
M4zamm1l <151562827+M4zamm1l@users.noreply.github.com>
2023-11-21 16:24:02 +0500

    Update M4Z1.py

--- M4Z1.py
+++ M4Z1.py
@@ -58,7 +58,6 @@ try:
  open('.prox.txt','w').write(prox)
 except Exception as e:
  print('')
-prox=open('.prox.txt','r').read().splitlines()
 for xd in range(10000):
     a='Nokia'
     b=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
